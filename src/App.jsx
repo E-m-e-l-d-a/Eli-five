8c621972150ca00b1c5bde12fc003300fee32cf4
unknown <marycynthiaugwu14@gmail.com>
2025-07-12 02:07:16 +0100

    tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-function App(){
-  return(
-    <div>
+function App() {
+  return (
+    <div className="flex-center divcol m-0 p-3">
       <h1>hello people!</h1>
+      <p>testing the app!</p>
     </div>
-  )
+  );
 }
+
 export default App;
