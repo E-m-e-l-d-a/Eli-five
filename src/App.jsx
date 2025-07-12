033f052738d535e840b1f58606d7f53fe038e899
unknown <marycynthiaugwu14@gmail.com>
2025-07-12 02:53:22 +0100

    tailwind update

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="flex-center divcol m-0 p-3">
-      <h1>hello people!</h1>
-      <p>testing the app!</p>
+      <h1>hello again!</h1>
     </div>
   );
 }
